94eefe194f5d7b0ec7db64a0a8f315c2e0e89138
mejvit <mejvit@gmail.com>
2021-04-21 15:12:10 +0200

    Instalace PWA elements do aplikace

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,10 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* PWA elements*/
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
+defineCustomElements(window);
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
